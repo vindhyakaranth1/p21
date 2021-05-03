22152ac80f39a90ec6a926cdb5c85a2080a1ceca
vindhyakaranth1 <78919752+vindhyakaranth1@users.noreply.github.com>
2021-05-03 21:09:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    // load sound here
+  music = loadSound ("music.mp3")
+
+
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(515,580,200,30);
+    block3.shapeColor = "purple";
+
+    block4 = createSprite(740,580,220,30);
+    block4.shapeColor = "black";
+
+    //create two more blocks i.e. block3 and block4 here
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    //write code to add velocityX and velocityY
+   ball.velocityX = 4
+   ball.velocityY = 6
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    
+    //write code to bounce off ball from the block1 
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = "blue";
+        music.play();
+    }
+
+
+
+    if(block2.isTouching(ball)){
+        ball.shapeColor = "orange";
+        //write code to set velocityX and velocityY of ball as 0
+        ball.velocityX = 0
+        ball.velocityY = 0
+        //write code to stop music
+        music.stop();
+    }
+
+    //write code to bounce off ball from the block3
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+        ball.shapeColor = "purple";
+        music.play();
+    }
+
+
+    //write code to bounce off ball from the block4
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+        ball.shapeColor = "black";
+        music.play();
+    }
+
+    drawSprites();
+}
